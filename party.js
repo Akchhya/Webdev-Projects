467b2e2ed10f94bb222452712d204046a106091a
akc <akc@gmail.com>
2024-05-07 17:18:07 +0530

    drums

--- party.js
+++
@@ -1,9 +0,0 @@
-//This is my first javascript program
-var guestName = prompt("What is your name?");
-var guest_List = ["Angela", "aishu", "aryan"];
-
-if (guest_List.includes(guestName.toLowerCase())) {
-    alert("Welcome to the party!");
-} else {
-    alert("Sorry, you are not on the guest list :(");
-}
