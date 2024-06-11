34b62bca6aadf3e170acf27d64a2dd0d35eada6f
akc <akc@gmail.com>
2024-06-11 18:28:44 +0530

    nodejs

---
+++ 2.4+QR+Code+Project/2.4 QR Code Project/index.js
@@ -0,0 +1,24 @@
+
+import fs from "fs";
+import inquirer from 'inquirer';
+import qr from "qr-image";
+
+
+inquirer
+  .prompt([
+    {"meassage": "Type your URL:",
+       name:"URL",
+      },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(require(fs.createWriteStream("qr_img.png")));// Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
